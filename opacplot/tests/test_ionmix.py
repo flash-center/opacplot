b5adc85c3f0d42b24ff37b98786d4ae08d5a9f7f
Bishara Korkor <Bishara.Korkor@gmail.com>
2012-07-10 11:13:29 -0400

    Nose tests for ionmix.py

--- opacplot/tests/test_ionmix.py
+++ opacplot/tests/test_ionmix.py
@@ -1,9 +1,20 @@
+import os
+
 import nose
 from nose.tools import assert_equal, assert_not_equal, assert_raises, raises, assert_in, \
-                       assert_true
+                       assert_true, with_setup
 
 import numpy as np
 from numpy.testing import assert_array_equal, assert_array_almost_equal
 
 from opacplot import ionmix
 
+def parser_teardown():
+    files = os.listdir('.')
+    h5files = [f for f in files if f.endswith('.h5')]
+    for f in h5files:
+        os.remove(f)
+
+@with_setup(lambda: None, parser_teardown)
+def test_parse1():
+    opp = ionmix.parse('imxtest.cn4')
